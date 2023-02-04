8dce4a8804fea5d366b014becf6041c110b8b5a0
Yishak Wondim <it.yisak.wondim@gmail.com>
2023-02-04 11:39:39 +0300

    models added to admin.py in users app

--- orbis_services/orbis_backend/settings.py
+++ orbis_services/orbis_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
 ]
 
 AUTH_USER_MODEL = "users.CustomUser" 
@@ -112,6 +114,22 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
+SIMPLE_JWT = {
+
+    'AUTH_HEADER_TYPES': ('JWT',),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
--- orbis_services/users/models.py
+++ orbis_services/users/models.py
@@ -3,12 +3,44 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 
+
+
+def upload_to(instance, filename):
+    return 'images/{filename}'.format(filename=filename)
+
+
+
+class Department(models.Model):
+    department_name = models.CharField(max_length=80)
+
+
 class CustomUser(AbstractUser):
-    username = models.CharField(max_length=120,unique=True, default="")
+    first_name = models.CharField(max_length=120, default="")
+    last_name = models.CharField(max_length=120,default="")
+    username = models.CharField(max_length=120)
     email = models.EmailField(max_length=100,default="")
     phone = models.CharField(max_length= 20,null= True)
-    
-    class Meta(AbstractUser.Meta):
-       swappable = 'AUTH_USER_MODEL'
+    profile = models.ImageField(upload_to= upload_to, null=True)
+    is_verified = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField()
+    is_customer = models.BooleanField()
+    is_company = models.BooleanField()
+    department = models.ForeignKey(Department,on_delete= models.PROTECT)
+
     def __str__(self) -> str:
         return self.get_username
+
+    def check_staff(self):
+        if self.is_customer == False:
+            self.is_staff == True
+        else:
+            self.is_staff == False
+
+
+
+    class Meta(AbstractUser.Meta):
+       swappable = 'AUTH_USER_MODEL'
+    
+
+
