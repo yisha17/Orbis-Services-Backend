f38fbbf6bf91d276a8bfa2e82e085452fa1ef942
Yishak Wondim <it.yisak.wondim@gmail.com>
2023-02-04 12:06:04 +0300

    adding car model in progress

--- orbis_services/sales/models.py
+++ orbis_services/sales/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cars(models.Model):
+    CAR_CHOICES = [
+        ('Mercedes','Mercedes'),
+        ('Renault','Renault')
+    ]
+
+    CAR_TYPES = [
+        ('Sedan', 'Sedan')
+    ]
+
+    FUEL = []
+
+    TRANSMISSION = []
+    
+    car_model = models.CharField(max_length=120)
+    car_brand = models.CharField(max_length=120, choices=CAR_CHOICES)
+    car_type = models.CharField(max_length= 80, choices=CAR_TYPES)
+    fuel_type = models.CharField(max_length= 120, choices= FUEL)
+    car_engine = models.CharField(max_length=120)
+    car_transmission =models.CharField(max_length= 120, choices= TRANSMISSION)
