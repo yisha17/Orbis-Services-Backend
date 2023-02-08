ba97090bbc8de623ac9d9c11ed968d32bf56655f
Yishak Wondim <it.yisak.wondim@gmail.com>
2023-02-08 16:27:22 +0300

    car model fields added

--- orbis_services/sales/models.py
+++ orbis_services/sales/models.py
@@ -10,16 +10,26 @@ class Cars(models.Model):
     ]
 
     CAR_TYPES = [
-        ('Sedan', 'Sedan')
+        ('Sedan', 'Sedan'),
+        ('Van','Van')
     ]
 
     FUEL = []
 
     TRANSMISSION = []
-    
+    # ------------Description----------
     car_model = models.CharField(max_length=120)
     car_brand = models.CharField(max_length=120, choices=CAR_CHOICES)
     car_type = models.CharField(max_length= 80, choices=CAR_TYPES)
+    seating_capacity = models.CharField(max_length= 200)
+    number_of_door = models.IntegerField()
+    # ------------Engine----------
     fuel_type = models.CharField(max_length= 120, choices= FUEL)
     car_engine = models.CharField(max_length=120)
+    horse_power = models.IntegerField()
+    emission_standard = models.CharField(max_length=100)
+    # ------------Transmission-----
     car_transmission =models.CharField(max_length= 120, choices= TRANSMISSION)
+    
+
+    
--- orbis_services/users/views.py
+++ orbis_services/users/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 from rest_framework import status
-# Create your views here.
+
 
 
 
